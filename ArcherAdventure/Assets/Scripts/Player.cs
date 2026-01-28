f2a223c27b285eb78886f6baee8f596bed040728
pwqxx03-art <pwqxx03@gmail.com>
2026-01-28 16:38:14 +0900

    플레이어 애니메이션

---
+++ ArcherAdventure/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("horizontal");
+        float VerticalInput = Input.GetAxisRaw("vertical");
+    }
+}
