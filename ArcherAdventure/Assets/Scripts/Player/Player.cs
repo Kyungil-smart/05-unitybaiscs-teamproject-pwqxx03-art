629c207ea86c2e6a7fa631f0c75cfae3db847936
pwqxx03-art <pwqxx03@gmail.com>
2026-01-29 14:56:59 +0900

    경계면 충돌시 덜덜 떠는 현상 보안

--- ArcherAdventure/Assets/Scripts/Player/Player.cs
+++ ArcherAdventure/Assets/Scripts/Player/Player.cs
@@ -5,13 +5,65 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchRight;
+    public bool isTouchLeft;
 
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if ((isTouchRight && horizontalInput == 1) || (isTouchLeft && horizontalInput == -1))
+            horizontalInput = 0;
+
         float verticalInput = Input.GetAxisRaw("Vertical");
+        if ((isTouchTop && verticalInput == 1) || (isTouchBottom && verticalInput == -1))
+            verticalInput = 0;
 
         Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f).normalized;
         transform.position += moveTo * moveSpeed * Time.deltaTime;
     }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Border"))
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+            }
+        }
+    }
+     void OnTriggerExit2D( Collider2D collision )
+     {
+        if (collision.gameObject.CompareTag("Border"))
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+            }
+        }
+    }
+        
 }
--- ArcherAdventure/Assets/Scripts/weapon/weapon.cs
+++ ArcherAdventure/Assets/Scripts/weapon/weapon.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class weapon : MonoBehaviour
-{
-    public float maxShotDelay;
-    public float curShotDelay;
-    public GameObject bulletobj;
+// public class weapon : MonoBehaviour
+// {
+//     public float maxShotDelay;
+//     public float curShotDelay;
+//     public GameObject bulletobj;
 
-    void Update()
-    {
-        Move();
-        fire();
-        boom();
-        reload();
+//     void Update()
+//     {
+//         Move();
+//         fire();
+//         boom();
+//         reload();
 
-    }
-}
+//     }
+//}
