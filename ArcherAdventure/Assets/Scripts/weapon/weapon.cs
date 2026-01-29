171e00002871f5b60326397d946b2da5055de8e2
pwqxx03-art <pwqxx03@gmail.com>
2026-01-29 12:31:55 +0900

    무기추가중

---
+++ ArcherAdventure/Assets/Scripts/weapon/weapon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public float maxShotDelay;
+    public float curShotDelay;
+    public GameObject bulletobj;
+
+    void Update()
+    {
+        Move();
+        fire();
+        boom();
+        reload();
+
+    }
+}
