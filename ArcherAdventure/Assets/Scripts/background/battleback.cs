11b60ebe51b3ac84a8f9546de181606935ac0d2b
pwqxx03-art <pwqxx03@gmail.com>
2026-01-29 11:10:08 +0900

    배경 움직임 및 폴더 정리

--- ArcherAdventure/Assets/Scripts/background/battleback.cs
+++ ArcherAdventure/Assets/Scripts/background/battleback.cs
@@ -9,5 +9,11 @@ public class battleback : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < -10) {
+            transform.position += new Vector3 ( 0 , 20f , 0 );
+        }
+        {
+            
+        }
     }
 }
